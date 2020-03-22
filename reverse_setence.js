21cfbadd2dea81e753c86488fd38ea61ae3063bf
regis <regis@rtmtag.com>
2020-03-22 19:59:13 -0300

    Reverse Setence

---
+++ reverse_setence.js
@@ -0,0 +1,14 @@
+/**
+Reverse the order of words in a given string sentence. 
+You can assume that sentence does not have any leading, trailing or repeating spaces.
+
+Example
+
+For sentence = "Man bites dog", the output should be
+reverseSentence(sentence) = "dog bites Man".
+ */
+function reverseSentence(sentence) {
+    return sentence.split(" ").reverse().join(" ");
+}
+
+console.log(reverseSentence("Man bites dog"));
